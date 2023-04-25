3cb2061c5a0c26de3d0309c33ff2b0fa64012265
Jenda <jan.illetsko@gmail.com>
2023-04-25 12:59:39 +0200

    homepage done

--- src/components/Content/HomeContent.jsx
+++ src/components/Content/HomeContent.jsx
@@ -6,14 +6,19 @@ export default function HomeContent () {
 
     return (
         <section className='home__container'>
-            <h5>So, you want to travel to</h5>
-            <h1>Space</h1>
-            <p>
-                Let’s face it; if you want to go to space, you might as well genuinely go to 
-                outer space and not hover kind of on the edge of it. Well sit back, and relax 
-                because we’ll give you a truly out of this world experience!
-            </p>
-            <Link to='/destination'><button>Explore</button></Link>
+            <div className="home__text">
+                <h5 className=''>SO, YOU WANT TO TRAVEL TO</h5>
+                <h1 className=''>SPACE</h1>
+                <p className=''>
+                    Let’s face it; if you want to go to space, you might as well genuinely go to 
+                    outer space and not hover kind of on the edge of it. Well sit back, and relax 
+                    because we’ll give you a truly out of this world experience!
+                </p> 
+            </div>
+            <Link className="home__button" to='/destination'><button>EXPLORE</button></Link>
+            
+            
+            
         </section>
     )
 }
